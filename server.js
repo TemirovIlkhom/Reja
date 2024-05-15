af4f8a4f3693b1a033ddd86892384698ef0e281c
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-05-15 20:52:16 +0900

    fead: tests

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
-const express = require("express");
-const app = express();
-const http = require("http");
+console.log("Wab serverni boshlash");
+const express = require("express"); // expressni chaqirib olyapmiz
+const res = require("express/lib/response");
+const app = express(); // app o'zgaruvchisiga expressni chaqirib oldik (aylantirib oldik express kk bolgan joyda appni ishlatsak boladi)
+const http = require("http"); // http ni chaqirish
 
-//1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// 1: Kirish code
+app.use(express.static("public")); // bu folderni ichida css va image/ boladi
+app.use(express.json()); // json farmat datani object xolatiga o'girib beradi
+app.use(express.urlencoded({ extended: true })); // bu codeni yozmasak formdan post qilingan narsa kelmaydi yani serverga kiritmaydi
 
-//2: Session code
+// 2: session code
 
-//3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
+// 3: Views code        BSSR
+app.set("views", "views"); // folderni ko'rsatayapmiz
+app.set("view engine", "ejs"); // views engine ejs ekanligini korsatib beryapmiz
 
-//4: Routing code
-app.get("/hi", function(req, res) {
-    res.end("<h1>Hello World by => Adam</h1>");
+// 4: Routing code
+app.post("create-item", (req, res) => {
+    console.log( req.body );
+    res.json({ test: "success" });
+});
+
+app.get('/', (req, res) => {
+    res.render("harid");
 });
 
 
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 3001; // partda 
 server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`the server is running successFull on post: ${PORT}`);
 });
+
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+ 
