d999992cceeb88661dd7406598d12f87f7ad7823
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-05-15 22:08:59 +0900

    fix: test

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 console.log("Wab serverni boshlash");
 const express = require("express"); // expressni chaqirib olyapmiz
-const res = require("express/lib/response");
 const app = express(); // app o'zgaruvchisiga expressni chaqirib oldik (aylantirib oldik express kk bolgan joyda appni ishlatsak boladi)
 const http = require("http"); // http ni chaqirish
 
 // 1: Kirish code
 app.use(express.static("public")); // bu folderni ichida css va image/ boladi
 app.use(express.json()); // json farmat datani object xolatiga o'girib beradi
-app.use(express.urlencoded({ extended: true })); // bu codeni yozmasak formdan post qilingan narsa kelmaydi yani serverga kiritmaydi
+app.use(express.urlencoded({ extended: true  })); // bu codeni yozmasak formdan post qilingan narsa kelmaydi yani serverga kiritmaydi
 
 // 2: session code
 
@@ -16,24 +15,24 @@ app.set("views", "views"); // folderni ko'rsatayapmiz
 app.set("view engine", "ejs"); // views engine ejs ekanligini korsatib beryapmiz
 
 // 4: Routing code
-app.post("create-item", (req, res) => {
+app.post("/create-item", (req, res) => {
     console.log( req.body );
     res.json({ test: "success" });
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render("harid");
 });
 
 
-const server = http.createServer(app);
-let PORT = 3001; // partda 
-server.listen(PORT, function() {
-    console.log(`the server is running successFull on post: ${PORT}`);
-});
-
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
+// const server = http.createServer(app);
+// let PORT = 3000; // partda 
+// server.listen(PORT, function() {
+//     console.log(`the server is running successFull on post: ${PORT}`);
 // });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
  
